654b488aa393ca2cff4713839af97996656d793e
omar bahri <bahri.omar69@gmail.com>
2025-12-01 13:28:53 +0100

    Initial backend setup with Express.js and MongoDB

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const app = require('./app');
+const connectDatabase = require('./config/database');
+
+const dotenv = require('dotenv');
+
+//setting up config file 
+dotenv.config({ path : 'config/config.env' });
+
+// Connecting to Database
+connectDatabase();
+
+const server = app.listen(process.env.PORT, ()=> {
+    console.log(`server started on PORT : ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+})
+
+// Handle Unhadled Promise Rejections
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down the server due to unhandled Promise rejection');
+    server.close(() => {
+        process.exit(1)
+    });
+})
